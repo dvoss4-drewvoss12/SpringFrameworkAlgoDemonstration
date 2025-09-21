96d8d074d4d37cb3aa7f3cf89991ca721175370f
dvoss4-drewvoss12 <drewvoss12@gmail.com>
2025-09-20 22:55:49 -0500

    Begin building custom error handling for webpage failures

---
+++ src/main/java/io/github/dvoss4/algoDemonstration/ErrorController.java
@@ -0,0 +1,11 @@
+package io.github.dvoss4.algoDemonstration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public String errorPage(){
+        return "errorx.html";
+    }
+}
--- src/main/java/io/github/dvoss4/algoDemonstration/WebController.java
+++ src/main/java/io/github/dvoss4/algoDemonstration/WebController.java
@@ -14,4 +14,10 @@ public class WebController {
         public String showFizzBuzzPage(){
             return "fizzbuzz.html";
         }
+        @GetMapping("/error")
+        public String showErrorPage(){
+            return "error.html";
+        }
 }
+
+
